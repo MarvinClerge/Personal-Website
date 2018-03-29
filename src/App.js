ed5f1b5b635410b44753df7e9377110c5ebe0174
Marvin Clerge <clergemarvin@gmail.com>
2018-03-29 12:53:12 -0400

    fixed about container in app js

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ class App extends Component {
           window.scrollTo(0,0);
           return(
             <div>
-              <AboutCont />
+              <AboutContainer />
             </div>
           )
         }}/>
