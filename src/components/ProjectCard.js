f6f55b63e54157e01982776406dd9c875f87a27c
Marvin Clerge <clergemarvin@gmail.com>
2018-04-24 18:09:28 -0400

    project section structure completed

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -9,8 +9,21 @@ class ProjectCard extends React.Component {
     }
   }
 
+  mapFeatures(){
+    return Object.keys(this.props.features).map((key, index) => {
+      return (
+        <div>
+          <h3 className='projectcard-title' >{key}</h3>
+          <p className='projectcard-paragraph' >{this.props.features[key]}</p>
+        </div>
+      )
+    })
+
+  }
+
   render(){
     console.log(this.props);
+
     return (
       <div className='projectcard'>
         <div className='projectcard-video'>
@@ -18,15 +31,22 @@ class ProjectCard extends React.Component {
             width="560"
             height="315"
             src={`https://www.youtube.com/embed/${this.props.videoID}`}
-            frameborder="0"
+            title={this.props.name}
+            frameBorder="0"
             allow="autoplay; encrypted-media"
-            allowfullscreen
+            allowFullScreen
           ></iframe>
         </div>
 
         <div className='projectcard-info'>
-          <h1>{this.props.name}</h1>
+          <h2>
+            {this.props.name}
+            <span><a href={this.props.frontend} target="_blank" rel="noopener noreferrer">
+              View on Github
+            </a></span>
+          </h2>
           <p>{this.props.description}</p>
+          {this.mapFeatures()}
         </div>
       </div>
     )
@@ -34,10 +54,3 @@ class ProjectCard extends React.Component {
 }
 
 export default ProjectCard
-
-{/* <p>{this.props.name}</p>
-<p>{this.props.date}</p>
-<p>{this.props.description}</p>
-<p>{this.props.frontend}</p>
-<p>{this.props.backend}</p>
-<p>{this.props.backend}</p> */}
